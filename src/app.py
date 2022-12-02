b976b8d63cd44f8a46dc38f30313f5f8b4f9f970
tim <timaltonji@protonmail.com>
2022-12-02 18:24:15 -0500

    pieces

--- src/app.py
+++ src/app.py
@@ -1,16 +1,79 @@
 from coordinate import Coordinate
+from pieces import rook, king, knight, pawn, queen, bishop
+import re
 
-active_player = 'white'
-game_board = []
+active_player = ''
+game_board_index = [[0 for x in range(8)] for y in range(8)]
+game_board = [[0 for x in range(8)] for y in range(8)]
 
-
-# while active_player is not 'none':
-    
-
+def create_game_board_index():
+    whiteturn = True
+    for y in range(8):
+        for x in range(8): 
+            if whiteturn:
+                game_board_index[x][y] = 'white'
+            else:
+                game_board_index[x][y] = 'black'
+            
+        whiteturn = not whiteturn
+        x = 0
+        
 def create_game_board():
-    j = 0
-    for i in range(64):
-        game_board[i] = Coordinate('white', )
+    for y in range(2):
+        for x in range(8):
+            if y == 1: 
+                game_board[x][y] = pawn('white', Coordinate(x,y))
+            if x == 0 and y ==0:
+                game_board[x][y] = knight('white', Coordinate(x,y))
+            if x == 1 and y ==0:
+                game_board[x][y] = rook('white', Coordinate(x,y))
+            if x == 2 and y ==0:
+                game_board[x][y] = bishop('white', Coordinate(x,y))
+            if x == 3 and y ==0:
+                game_board[x][y] = queen('white', Coordinate(x,y))
+            if x == 4 and y ==0:
+                game_board[x][y] = king('white', Coordinate(x,y))
+            if x == 5 and y ==0:
+                game_board[x][y] = bishop('white', Coordinate(x,y))
+            if x == 6 and y ==0:
+                game_board[x][y] = rook('white', Coordinate(x,y))
+            if x == 7 and y ==0:
+                game_board[x][y] = knight('white', Coordinate(x,y))
+    
+    for y in range(6,8):
+        for x in range(8):
+            if y == 6: 
+                game_board[x][y] = pawn('black', Coordinate(x,y))
+            if x == 0 and y ==7:
+                game_board[x][y] = knight('black', Coordinate(x,y))
+            if x == 1 and y ==7:
+                game_board[x][y] = rook('black', Coordinate(x,y))
+            if x == 2 and y ==7:
+                game_board[x][y] = bishop('black', Coordinate(x,y))
+            if x == 3 and y ==7:
+                game_board[x][y] = queen('black', Coordinate(x,y))
+            if x == 4 and y ==7:
+                game_board[x][y] = king('black', Coordinate(x,y))
+            if x == 5 and y ==7:
+                game_board[x][y] = bishop('black', Coordinate(x,y))
+            if x == 6 and y ==7:
+                game_board[x][y] = rook('black', Coordinate(x,y))
+            if x == 7 and y ==7:
+                game_board[x][y] = knight('black', Coordinate(x,y))
 
-def turn_rotation():
-    print(f'{active_player}\'s move')
+def start_prompt():
+    print("White or black? ")
+    active_player = input()
+    pattern = "[WwHhIiTtEe]|[BbLlAaCcKk]"
+    if re.search(pattern, active_player):
+        print(active_player.lower()+"\'s move")
+    else:
+        print("please make a valid selection.")
+        start_prompt()
+
+def start():
+    start_prompt()
+    create_game_board_index()
+    create_game_board()
+
+start()
---
+++ src/pieces.py
@@ -0,0 +1,64 @@
+class pawn():
+    name = 'pawn'
+
+    def __init__(self, color, coordinate) -> None:
+        self.color = color
+        self.coordinate = coordinate
+
+    def move(self):
+        print('nice move')
+
+
+class knight():
+    name = 'knight'
+
+    def __init__(self, color, coordinate) -> None:
+        self.color = color
+        self.coordinate = coordinate
+
+    def move(self):
+        print('nice move')
+
+
+class rook():
+    name = 'rook'
+
+    def __init__(self, color, coordinate) -> None:
+        self.color = color
+        self.coordinate = coordinate
+
+    def move(self):
+        print('nice move')
+
+
+class bishop():
+    name = 'bishop'
+
+    def __init__(self, color, coordinate) -> None:
+        self.color = color
+        self.coordinate = coordinate
+
+    def move(self):
+        print('nice move')
+
+
+class king():
+    name = 'king'
+
+    def __init__(self, color, coordinate) -> None:
+        self.color = color
+        self.coordinate = coordinate
+
+    def move(self):
+        print('nice move')
+
+
+class queen():
+    name = 'queen'
+
+    def __init__(self, color, coordinate) -> None:
+        self.color = color
+        self.coordinate = coordinate
+
+    def move(self):
+        print('nice move')
