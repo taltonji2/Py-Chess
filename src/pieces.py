7d9e68dfefa17237d13398721bfcac8c6698ec9a
tim <timaltonji@protonmail.com>
2022-12-30 14:11:52 -0500

    feat: piece movement with more rules

--- src/pieces.py
+++ src/pieces.py
@@ -42,25 +42,28 @@ class Pawn(Piece):
         y = self.coordinate.y
 
         if(self.color == 'black'):
-            if (x-1 > -1 and y-1 > -1):
+            if (x-1 > -1 and y-1 > -1 and gb[x-1][y-1].color == 'white' or gb[x-1][y-1] == 0):
                 upper_left_coordinate = Coordinate(x-1, y-1)
                 self.legal_moves.add(upper_left_coordinate)
-            if (y-1 > -1):
+            if (y-1 > -1 and gb[x-1][y-1] == 0):
                 up_coordinate = Coordinate(x, y-1)
                 self.legal_moves.add(up_coordinate)
-            if (x+1 < 8 and y-1 > -1):
+            if (x+1 < 8 and y-1 > -1 and gb[x-1][y-1].color == 'white' or gb[x-1][y-1] == 0):
                 upper_right_coordinate = Coordinate(self.coordinate.x+1, self.coordinate.y-1)
                 self.legal_moves.add(upper_right_coordinate)
         else:
-            if (x-1 > -1 and y+1 < 8):
+            if (x-1 > -1 and y+1 < 8 and gb[x-1][y-1].color == 'black' or gb[x-1][y-1] == 0):
                 upper_left_coordinate = Coordinate(self.coordinate.x-1, self.coordinate.y+1)
                 self.legal_moves.add(upper_left_coordinate)
-            if (y+1 < 8):
+            if (y+1 < 8 and  gb[x-1][y-1] == 0):
                 up_coordinate = Coordinate(self.coordinate.x, self.coordinate.y+1)
                 self.legal_moves.add(up_coordinate)
-            if (x+1< 8 and y+1 < 8):
+            if (x+1< 8 and y+1 < 8 and gb[x-1][y-1].color == 'black' or gb[x-1][y-1] == 0):
                 upper_right_coordinate = Coordinate(self.coordinate.x+1, self.coordinate.y+1)
                 self.legal_moves.add(upper_right_coordinate)
+    
+    def promote(self, piece):
+        self.legal_moves.clear()
 
 class Rook(Piece):
     name = 'rook'
@@ -76,19 +79,43 @@ class Rook(Piece):
 
         for xi in range(x):
             left_coordinate = Coordinate(xi, y)
-            self.legal_moves.add(left_coordinate)
+            if gb[xi][y] == 0:
+                self.legal_moves.add(left_coordinate)
+            if gb[xi][y] != 0 and gb[xi][y].color != self.color:
+                self.legal_moves.add(left_coordinate)
+                break
+            else:
+                break
 
         for yi in range(0, y):
             up_coordinate = Coordinate(x, yi)
-            self.legal_moves.add(up_coordinate)
+            if gb[x][yi]==0:
+                self.legal_moves.add(up_coordinate)
+            if gb[xi][y] != 0 and gb[x][yi].color != self.color:
+                self.legal_moves.add(up_coordinate)
+                break
+            else:
+                break
 
         for xi in range(x, 8):
             right_coordinate = Coordinate(xi, y)
-            self.legal_moves.add(right_coordinate)
+            if gb[xi][y] == 0:
+                self.legal_moves.add(right_coordinate)
+            if gb[xi][y] != 0 and gb[xi][y].color != self.color:
+                self.legal_moves.add(right_coordinate)
+                break
+            else:
+                break
 
         for yi in range(y, 8):
-                down_coordinate = Coordinate(x, yi)
+            down_coordinate = Coordinate(x, yi)
+            if gb[x][yi] == 0:
                 self.legal_moves.add(down_coordinate)
+            if gb[x][yi] != 0 and gb[x][yi].color != self.color:
+                self.legal_moves.add(down_coordinate)
+                break
+            else:
+                break
 
 class Knight(Piece):
     name = 'knight'
@@ -111,22 +138,47 @@ class Knight(Piece):
         down_left = Coordinate(x-1, y+2)
         down_right = Coordinate(x+1, y+2)
         
-        if x-1 > -1 and y-2 > -1:
-            self.legal_moves.add(up_left)
+        if x-1>-1 and y-2>-1:
+            if gb[x-1][y-2] == 0:
+                self.legal_moves.add(up_left)
+            if gb[x-1][y-2] != 0 and gb[x-1][y-2].color != self.color:
+                self.legal_moves.add(up_left)
         if x+1<8 and y-2>-1:
-            self.legal_moves.add(up_right)
-        if x-2 > -1 and y-1 > -1:
-            self.legal_moves.add(left_up)
-        if x-2 > -1 and y+1 < 8:
-            self.legal_moves.add(left_down)
-        if x+2 < 8 and y-1 > -1:
-            self.legal_moves.add(right_up)
-        if x+2 < 8 and y+1 < 8:
-            self.legal_moves.add(right_down)
-        if x-1 > -1 and y+2 < 8:
-            self.legal_moves.add(down_left)
-        if x+1 < 8 and y+2 < 8:
-            self.legal_moves.add(down_right)
+            if gb[x+1][y-2] == 0:
+                self.legal_moves.add(up_right)
+            if gb[x+1][y-2] != 0 and gb[x-1][y-2].color != self.color:
+                self.legal_moves.add(up_right)
+        if x-2>-1 and y-1>-1:
+            if gb[x-2][y-1] == 0:
+                self.legal_moves.add(left_up)
+            if gb[x-2][y-1] != 0 and gb[x-2][y-1].color != self.color:
+                self.legal_moves.add(left_up)
+        if x-2>-1 and y+1<8:
+            if gb[x-2][y+1] == 0:
+                self.legal_moves.add(left_down)
+            if gb[x-2][y+1] != 0 and gb[x-2][y+1].color != self.color:
+                self.legal_moves.add(left_down)
+        if x+2<8 and y-1>-1:
+            if gb[x+2][y-1] == 0:
+                self.legal_moves.add(right_up)
+            if gb[x+2][y-1] != 0 and gb[x+2][y-1].color != self.color:
+                self.legal_moves.add(right_up)
+        if x+2<8 and y+1<8:
+            if gb[x+2][y+1] == 0:
+                self.legal_moves.add(right_down)
+            if gb[x+2][y+1] != 0 and gb[x+2][y+1].color != self.color:
+                self.legal_moves.add(right_down)
+        if x-1>-1 and y+2<8:
+            if gb[x-1][y+2] == 0:
+                self.legal_moves.add(down_left)
+            if gb[x-1][y+2] != 0 and gb[x-1][y+2].color != self.color:
+                self.legal_moves.add(down_left)
+        if x+1<8 and y+2<8:
+            if gb[x+1][y+2] == 0:
+                self.legal_moves.add(down_right)
+            if gb[x+1][y+2] != 0 and gb[x+1][y+2].color != self.color:
+                self.legal_moves.add(down_right)
+            
 
 class Bishop(Piece):
     name = 'bishop'
@@ -142,25 +194,49 @@ class Bishop(Piece):
             x -= 1
             y -= 1
             up_left_diagonal = Coordinate(x, y)
-            self.legal_moves.add(up_left_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(up_left_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(up_left_diagonal)
+                break
+            else:
+                break
         x = self.coordinate.x, y = self.coordinate.y
         while x < 8 and y > -1:
             x += 1
             y -= 1
             up_right_diagonal = Coordinate(x, y)
-            self.legal_moves.add(up_right_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(up_right_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(up_right_diagonal)
+                break
+            else:
+                break
         x = self.coordinate.x, y = self.coordinate.y
         while x > -1 and y < 8:
             x -= 1
             y += 1
             down_left_diagonal = Coordinate(x, y)
-            self.legal_moves.add(down_left_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(down_left_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(down_left_diagonal)
+                break
+            else:
+                break
         x = self.coordinate.x, y = self.coordinate.y
         while x < 8 and y < 8:
             x += 1
             y += 1
             down_right_diagonal = Coordinate(x, y)
-            self.legal_moves.add(down_right_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(down_right_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(down_right_diagonal)
+                break
+            else:
+                break
         
 class Queen(Piece):
     name = 'queen'
@@ -171,48 +247,95 @@ class Queen(Piece):
 
     def create_move_set(self, gb):
         self.legal_moves.clear()
-
         x = self.coordinate.x, y = self.coordinate.y
-        
+
         for xi in range(x):
             left_coordinate = Coordinate(xi, y)
-            self.legal_moves.add(left_coordinate)
+            if gb[xi][y] == 0:
+                self.legal_moves.add(left_coordinate)
+            if gb[xi][y] != 0 and gb[xi][y].color != self.color:
+                self.legal_moves.add(left_coordinate)
+                break
+            else:
+                break
 
         for yi in range(0, y):
             up_coordinate = Coordinate(x, yi)
-            self.legal_moves.add(up_coordinate)
+            if gb[x][yi]==0:
+                self.legal_moves.add(up_coordinate)
+            if gb[xi][y] != 0 and gb[x][yi].color != self.color:
+                self.legal_moves.add(up_coordinate)
+                break
+            else:
+                break
 
         for xi in range(x, 8):
             right_coordinate = Coordinate(xi, y)
-            self.legal_moves.add(right_coordinate)
+            if gb[xi][y] == 0:
+                self.legal_moves.add(right_coordinate)
+            if gb[xi][y] != 0 and gb[xi][y].color != self.color:
+                self.legal_moves.add(right_coordinate)
+                break
+            else:
+                break
 
         for yi in range(y, 8):
-                down_coordinate = Coordinate(x, yi)
+            down_coordinate = Coordinate(x, yi)
+            if gb[x][yi] == 0:
                 self.legal_moves.add(down_coordinate)
-
+            if gb[x][yi] != 0 and gb[x][yi].color != self.color:
+                self.legal_moves.add(down_coordinate)
+                break
+            else:
+                break
+        
         while x > -1 and y > -1:
             x -= 1
             y -= 1
             up_left_diagonal = Coordinate(x, y)
-            self.legal_moves.add(up_left_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(up_left_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(up_left_diagonal)
+                break
+            else:
+                break
         x = self.coordinate.x, y = self.coordinate.y
         while x < 8 and y > -1:
             x += 1
             y -= 1
             up_right_diagonal = Coordinate(x, y)
-            self.legal_moves.add(up_right_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(up_right_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(up_right_diagonal)
+                break
+            else:
+                break
         x = self.coordinate.x, y = self.coordinate.y
         while x > -1 and y < 8:
             x -= 1
             y += 1
             down_left_diagonal = Coordinate(x, y)
-            self.legal_moves.add(down_left_diagonal)
+            if gb[x][y] == 0:
+                self.legal_moves.add(down_left_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(down_left_diagonal)
+                break
+            else:
+                break
         x = self.coordinate.x, y = self.coordinate.y
         while x < 8 and y < 8:
             x += 1
             y += 1
             down_right_diagonal = Coordinate(x, y)
-            self.legal_moves.add(down_right_diagonal)   
+            if gb[x][y] == 0:
+                self.legal_moves.add(down_right_diagonal)
+            if gb[x][y] != 0 and gb[x][y].color != self.color:
+                self.legal_moves.add(down_right_diagonal)
+                break
+            else:
+                break
 
 class King(Piece):
     name = 'king'
@@ -226,25 +349,33 @@ class King(Piece):
         x = self.coordinate.x, y = self.coordinate.y
         if x-1>-1 and y-1 >-1:
             upper_left = Coordinate(x-1, y-1)
-            self.legal_moves.add(upper_left)
+            if gb[x-1][y-1] == 0 or gb[x-1][y-1].color != self.color:
+                self.legal_moves.add(upper_left)
         if y-1>-1:
             up = Coordinate(x, y-1)
-            self.legal_moves.add(up)
+            if gb[x][y-1] == 0 or gb[x][y-1].color != self.color:
+                self.legal_moves.add(up)
         if x+1<8 and y-1>-1:
             upper_right = Coordinate(x+1, y-1)
-            self.legal_moves.add(upper_right)
+            if gb[x+1][y-1] == 0 or gb[x+1][y-1].color != self.color:
+                self.legal_moves.add(upper_right)
         if x-1>-1:
             left = Coordinate(x-1, y)
-            self.legal_moves.add(left)
+            if gb[x-1][y] == 0 or gb[x-1][y].color != self.color:
+                self.legal_moves.add(left)
         if x+1<8:
             right = Coordinate(x+1, y)
-            self.legal_moves.add(right)
+            if gb[x+1][y] == 0 or gb[x+1][y].color != self.color:
+                self.legal_moves.add(right)
         if x-1>-1 and y+1<8:
             lower_left = Coordinate(x-1, y+1)
-            self.legal_moves.add(lower_left)
+            if gb[x-1][y+1] == 0 or gb[x-1][y+1].color != self.color:
+                self.legal_moves.add(lower_left)
         if x+1<8 and y+1<8:
             lower_right = Coordinate(x+1, y+1)
-            self.legal_moves.add(lower_right)
+            if gb[x+1][y+1] == 0 or gb[x+1][y+1].color != self.color:
+                self.legal_moves.add(lower_right)
         if y+1<8:
             down = Coordinate(x, y+1)
-            self.legal_moves.add(down)
+            if gb[x][y+1] == 0 or gb[x][y+1].color != self.color:
+                self.legal_moves.add(down)
